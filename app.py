641260c03177143cd00a4806b97e5654c0d06568
keelw <wjordankeel@gmail.com>
2024-08-08 02:39:39 +0000

    Editing python app.py file

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, session
+
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    if 'counter' not in session:
+        session['counter'] = 0
+    return render_template('index.html', counter=session['counter'])
+
+@app.route('/increment', methods=['POST'])
+def increment():
+    session['counter'] += 1
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
